f49b71aeb6412c3d846645b0a547589b68ce4e95
TuanViet <vietlt226688@gmail.com>
2023-10-12 12:58:41 +0700

    style:  category page and category create page

--- src/pages/admin/categoryAdd/index.tsx
+++ src/pages/admin/categoryAdd/index.tsx
@@ -1,7 +1,86 @@
+import React from 'react';
+import type { FormInstance } from 'antd';
+import {
+    Breadcrumb,
+    Button,
+    Form,
+    Input,
+    Space,
+} from 'antd';
+import { UploadOutlined } from "@ant-design/icons";
+import { Link, useNavigate } from 'react-router-dom';
+import Dragger from 'antd/es/upload/Dragger';
+
+const SubmitButton = ({ form }: { form: FormInstance }) => {
+    const [submittable, setSubmittable] = React.useState(false);
+
+    // Watch all values
+    const values = Form.useWatch([], form);
+
+    React.useEffect(() => {
+        form.validateFields({ validateOnly: true }).then(
+            () => {
+                setSubmittable(true);
+            },
+            () => {
+                setSubmittable(false);
+            },
+        );
+    }, [values]);
+
+    return (
+        <Button type="primary" htmlType="submit" disabled={!submittable} className='bg-blue-500'>
+            Submit
+        </Button>
+    );
+};
 
 const categoryAdd = () => {
+    const [form] = Form.useForm();
+
     return <>
-        <h2>categoryAdd</h2>
+        <Breadcrumb className='pb-3'
+            items={[
+
+                {
+                    title: <Link to={`/admin/category`}>Category</Link>,
+                },
+                {
+                    title: 'Add Category',
+                },
+            ]}
+        />
+        <div className="border p-10 rounded-lg  bg-white">
+            <h3 className="text-center text-2xl font-bold uppercase text-[#1677ff] pb-5">
+                Create New Category
+            </h3>
+            <Form
+                form={form}
+                name="validateOnly"
+                layout="vertical"
+                autoComplete="off"
+                className="mx-auto max-w-[500px]"
+            >
+                {/* Name Category */}
+                <Form.Item name="name" label="Category Name" rules={[{ required: true, message: 'Please input your Name!' }]}>
+                    <Input />
+                </Form.Item>
+
+                {/* Upload Images */}
+                <Form.Item label="Images" name="images" rules={[{ required: true, message: 'Please input your Image!' }]}>
+                    <Dragger>
+                        <Button icon={<UploadOutlined />}>Choose images</Button>
+                    </Dragger>
+                </Form.Item>
+                <Form.Item>
+                    <Space>
+                        <SubmitButton form={form} />
+                        <Button htmlType="reset">Reset</Button>
+                    </Space>
+                </Form.Item>
+            </Form>
+        </div>
+
     </>
 }
 export default categoryAdd;
--- src/pages/admin/categoryPage/index.tsx
+++ src/pages/admin/categoryPage/index.tsx
@@ -1,7 +1,83 @@
+import { Space, Table, Button, Popconfirm, } from 'antd';
+import {
+    EditFilled,
+    DeleteFilled,
+    PlusOutlined
+} from '@ant-design/icons';
+import { Link } from "react-router-dom";
 
 const categoryPage = () => {
-    return <>
-        <h2>categoryPage</h2>
-    </>
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text: String) => <a>{text}</a>,
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (record: any) => (
+                <Space size="middle">
+                    <Popconfirm
+                        title="Delete category"
+                        description="Are you sure to delete this category?"
+                        // onConfirm={() => confirm(record.key)}
+                        okText="Yes"
+                        cancelText="No"
+                        okButtonProps={{ className: "text-white bg-blue-400" }}
+                    >
+                        <DeleteFilled className='text-xl text-red-400' />
+                    </Popconfirm>
+                    <Link to={`/admin/category/update/${record.key}`}>
+                        <EditFilled className='text-xl text-yellow-400' />
+                    </Link>
+                </Space>
+            ),
+        },
+
+    ];
+
+    const data = [
+        {
+            key: '1',
+            name: 'Quần áo mùa hè',
+        },
+        {
+            key: '2',
+            name: 'Quần áo mùa đông',
+
+        },
+        {
+            key: '3',
+            name: 'Polo',
+
+        },
+    ];
+
+    return (
+        <div className="">
+            <Space className='flex justify-between mb-5'>
+                <div className="">
+                    <span className="block text-xl text-[#1677ff]">
+                        Category List
+                    </span>
+                    <span className="block text-base  text-[#1677ff]">
+                        Manage your categorys
+                    </span>
+                </div>
+                <Link to={`add`}>
+                    <Button type='primary' className='bg-blue-500'
+                        icon={<PlusOutlined />}
+                    >
+                        Add New Category
+                    </Button>
+                </Link>
+            </Space>
+            <div className="border p-3 rounded-lg  bg-white">
+                <Table columns={columns} dataSource={data} />
+            </div>
+        </div>
+    )
 }
 export default categoryPage;
