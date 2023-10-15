0026d4a588ea596483ea3dc05e35f62634a37159
Rioedev <lequy11082003@gmail.com>
2023-10-15 13:02:19 +0700

    style: homepage

---
+++ src/pages/client/homePage/Category.tsx
@@ -0,0 +1,252 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const Category = () => {
+  return (
+    <div className="mt-24 max-w-[1500px] mx-auto mb-[80px]">
+      <h1 className="text-[37px] font-semibold uppercase mb-8">
+        Danh mục sản phẩm
+      </h1>
+      <div className="category-list">
+        <Swiper
+          grabCursor={"true"}
+          spaceBetween={30}
+          slidesPerView={"auto"}
+          pagination={{ clickable: true, dynamicBullets: true }}
+        >
+          <SwiperSlide>
+            <div className="relative">
+              <Link to="">
+                <img
+                  src="/images/img-product/home_category_1_img.png"
+                  className="mx-auto transition-all hover:scale-105"
+                  alt=""
+                />
+              </Link>
+              <div className="flex justify-between items-center w-[352.5px] bg-gray-200 bg-opacity-40 p-5 absolute bottom-0">
+                <Link
+                  to=""
+                  className="text-[22px] font-medium leading-none hover:text-primary transition-all"
+                >
+                  Áo Polo
+                </Link>
+                <Link
+                  to=""
+                  className="w-[45px] h-[45px] rounded-full bg-white hover:bg-black hover:text-white flex justify-center items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <Link to="">
+                <img
+                  src="/images/img-product/home_category_1_img.png"
+                  className="mx-auto transition-all hover:scale-105"
+                  alt=""
+                />
+              </Link>
+              <div className="flex justify-between items-center w-[352.5px] bg-gray-200 bg-opacity-40 p-5 absolute bottom-0">
+                <Link
+                  to=""
+                  className="text-[22px] font-medium leading-none hover:text-primary transition-all"
+                >
+                  Áo Polo
+                </Link>
+                <Link
+                  to=""
+                  className="w-[45px] h-[45px] rounded-full bg-white hover:bg-black hover:text-white flex justify-center items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <Link to="">
+                <img
+                  src="/images/img-product/home_category_1_img.png"
+                  className="mx-auto transition-all hover:scale-105"
+                  alt=""
+                />
+              </Link>
+              <div className="flex justify-between items-center w-[352.5px] bg-gray-200 bg-opacity-40 p-5 absolute bottom-0">
+                <Link
+                  to=""
+                  className="text-[22px] font-medium leading-none hover:text-primary transition-all"
+                >
+                  Áo Polo
+                </Link>
+                <Link
+                  to=""
+                  className="w-[45px] h-[45px] rounded-full bg-white hover:bg-black hover:text-white flex justify-center items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <Link to="">
+                <img
+                  src="/images/img-product/home_category_1_img.png"
+                  className="mx-auto transition-all hover:scale-105"
+                  alt=""
+                />
+              </Link>
+              <div className="flex justify-between items-center w-[352.5px] bg-gray-200 bg-opacity-40 p-5 absolute bottom-0">
+                <Link
+                  to=""
+                  className="text-[22px] font-medium leading-none hover:text-primary transition-all"
+                >
+                  Áo Polo
+                </Link>
+                <Link
+                  to=""
+                  className="w-[45px] h-[45px] rounded-full bg-white hover:bg-black hover:text-white flex justify-center items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <Link to="">
+                <img
+                  src="/images/img-product/home_category_1_img.png"
+                  className="mx-auto transition-all hover:scale-105"
+                  alt=""
+                />
+              </Link>
+              <div className="flex justify-between items-center w-[352.5px] bg-gray-200 bg-opacity-40 p-5 absolute bottom-0">
+                <Link
+                  to=""
+                  className="text-[22px] font-medium leading-none hover:text-primary transition-all"
+                >
+                  Áo Polo
+                </Link>
+                <Link
+                  to=""
+                  className="w-[45px] h-[45px] rounded-full bg-white hover:bg-black hover:text-white flex justify-center items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <Link to="">
+                <img
+                  src="/images/img-product/home_category_1_img.png"
+                  className="mx-auto transition-all hover:scale-105"
+                  alt=""
+                />
+              </Link>
+              <div className="flex justify-between items-center w-[352.5px] bg-gray-200 bg-opacity-40 p-5 absolute bottom-0">
+                <Link
+                  to=""
+                  className="text-[22px] font-medium leading-none hover:text-primary transition-all"
+                >
+                  Áo Polo
+                </Link>
+                <Link
+                  to=""
+                  className="w-[45px] h-[45px] rounded-full bg-white hover:bg-black hover:text-white flex justify-center items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
--- src/pages/client/homePage/index.tsx
+++ src/pages/client/homePage/index.tsx
@@ -1,11 +1,21 @@
 import Banner from "../../../layout/Banner";
 import Footer from "../../../layout/Footer";
 import Header from "../../../layout/Header";
+import Category from "./Category";
+import Outstanding_Product from "./Outstanding_Product";
+import Policy from "./Policy";
+import Product_Collection from "./Product_Collection";
+import Sale from "./Sale";
 
 const homePage = () => {
     return <>
         <Header></Header>
         <Banner></Banner>
+        <Category></Category>
+        <Sale></Sale>
+        <Outstanding_Product></Outstanding_Product>
+        <Product_Collection></Product_Collection>
+        <Policy></Policy>
         <Footer></Footer>
     </>
 }
