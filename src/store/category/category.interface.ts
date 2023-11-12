0266aaf9529a2583a696c9bb68d1012f8c8dfd80
quyenncph25762 <viponline1610203@gmail.com>
2023-11-12 16:34:29 +0700

    fix: interface category

--- src/store/category/category.interface.ts
+++ src/store/category/category.interface.ts
@@ -3,7 +3,12 @@ import IProduct from "../product/product.interface"
 export interface ICategory {
     _id?: string
     products: IProduct[]
+    images: url[]
     name: string
+}
+
+export interface url {
+    url: string
 }
 
 export interface ICategoryState {
