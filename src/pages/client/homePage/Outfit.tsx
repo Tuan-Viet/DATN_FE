1cf36b2a261afe4bede982b221a23d66e724d06d
Rioedev <lequy11082003@gmail.com>
2023-12-11 15:52:07 +0700

    outfit of the day

---
+++ src/pages/client/homePage/Outfit.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Outfit = () => {
+  return (
+    <div className="bg-[#faefec] py-[50px]">
+      <div className="container">
+        <h1 className="text-[37px] mb-6 font-semibold uppercase">
+          Outfit of the day
+        </h1>
+        <div className="grid grid-cols-3 gap-x-5">
+          <div className="">
+            <div className="outfit group transition-all relative">
+              <img
+                src="/images/img-product/combo/set_combo_1.png"
+                alt=""
+                className="w-[476px] h-[677px] object-cover mb-4"
+              />
+              <p className="uppercase text-xl mb-4 font-medium">
+                Outfit đi làm
+              </p>
+              <button className="uppercase bg-white text-black border border-black rounded py-3 px-6 hover:text-white hover:bg-black transition-all">
+                Mua fullset
+              </button>
+              <div className="hidden group-hover:block transition-all absolute top-[25%] left-[20%]">
+                <Link to="" className="flex mb-5 gap-x-4 max-w-[277px] bg-white rounded-md p-3">
+                  <img
+                    src="/images/img-product/so-mi/tron/dai-tay/20220916_ya0hnmgf0o4pibssy9iti4su_118cdff5d2664ea4af32eea292c9e2b3_master.jpg"
+                    alt=""
+                    className="w-[75px] h-[100px] object-cover flex-shrink-0"
+                  />
+                  <div>
+                    <Link to="" className="block font-semibold mb-2">
+                      Sơ mi tay dài trơn Bamboo EATB041
+                    </Link>
+                    <span className="text-red-500 font-semibold">450,000₫</span>
+                  </div>
+                </Link>
+                <Link to="" className="flex gap-x-4 max-w-[277px] bg-white rounded-md p-3">
+                  <img
+                    src="/images/img-product/Quan-dai/jeans/20230209_2nqi04eclesynryo_614790fe9b5a479095c6726ef90f2df7_master.jpg"
+                    alt=""
+                    className="w-[75px] h-[100px] object-cover flex-shrink-0"
+                  />
+                  <div>
+                    <Link to="" className="block font-semibold mb-2">
+                      Quần Jeans basic slim EABJ012
+                    </Link>
+                    <span className="text-red-500 font-semibold">500,000₫</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="outfit group transition-all relative">
+              <img
+                src="/images/img-product/combo/set_combo_1.png"
+                alt=""
+                className="w-[476px] h-[677px] object-cover mb-4"
+              />
+              <p className="uppercase text-xl mb-4 font-medium">
+                Outfit đi làm
+              </p>
+              <button className="uppercase bg-white text-black border border-black rounded py-3 px-6 hover:text-white hover:bg-black transition-all">
+                Mua fullset
+              </button>
+              <div className="hidden group-hover:block transition-all absolute top-[25%] left-[20%]">
+                <Link to="" className="flex mb-5 gap-x-4 max-w-[277px] bg-white rounded-md p-3">
+                  <img
+                    src="/images/img-product/so-mi/tron/dai-tay/20220916_ya0hnmgf0o4pibssy9iti4su_118cdff5d2664ea4af32eea292c9e2b3_master.jpg"
+                    alt=""
+                    className="w-[75px] h-[100px] object-cover flex-shrink-0"
+                  />
+                  <div>
+                    <Link to="" className="block font-semibold mb-2">
+                      Sơ mi tay dài trơn Bamboo EATB041
+                    </Link>
+                    <span className="text-red-500 font-semibold">450,000₫</span>
+                  </div>
+                </Link>
+                <Link to="" className="flex gap-x-4 max-w-[277px] bg-white rounded-md p-3">
+                  <img
+                    src="/images/img-product/Quan-dai/jeans/20230209_2nqi04eclesynryo_614790fe9b5a479095c6726ef90f2df7_master.jpg"
+                    alt=""
+                    className="w-[75px] h-[100px] object-cover flex-shrink-0"
+                  />
+                  <div>
+                    <Link to="" className="block font-semibold mb-2">
+                      Quần Jeans basic slim EABJ012
+                    </Link>
+                    <span className="text-red-500 font-semibold">500,000₫</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="outfit group transition-all relative">
+              <img
+                src="/images/img-product/combo/set_combo_1.png"
+                alt=""
+                className="w-[476px] h-[677px] object-cover mb-4"
+              />
+              <p className="uppercase text-xl mb-4 font-medium">
+                Outfit đi làm
+              </p>
+              <button className="uppercase bg-white text-black border border-black rounded py-3 px-6 hover:text-white hover:bg-black transition-all">
+                Mua fullset
+              </button>
+              <div className="hidden group-hover:block transition-all absolute top-[25%] left-[20%]">
+                <Link to="" className="flex mb-5 gap-x-4 max-w-[277px] bg-white rounded-md p-3">
+                  <img
+                    src="/images/img-product/so-mi/tron/dai-tay/20220916_ya0hnmgf0o4pibssy9iti4su_118cdff5d2664ea4af32eea292c9e2b3_master.jpg"
+                    alt=""
+                    className="w-[75px] h-[100px] object-cover flex-shrink-0"
+                  />
+                  <div>
+                    <Link to="" className="block font-semibold mb-2">
+                      Sơ mi tay dài trơn Bamboo EATB041
+                    </Link>
+                    <span className="text-red-500 font-semibold">450,000₫</span>
+                  </div>
+                </Link>
+                <Link to="" className="flex gap-x-4 max-w-[277px] bg-white rounded-md p-3">
+                  <img
+                    src="/images/img-product/Quan-dai/jeans/20230209_2nqi04eclesynryo_614790fe9b5a479095c6726ef90f2df7_master.jpg"
+                    alt=""
+                    className="w-[75px] h-[100px] object-cover flex-shrink-0"
+                  />
+                  <div>
+                    <Link to="" className="block font-semibold mb-2">
+                      Quần Jeans basic slim EABJ012
+                    </Link>
+                    <span className="text-red-500 font-semibold">500,000₫</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Outfit;
